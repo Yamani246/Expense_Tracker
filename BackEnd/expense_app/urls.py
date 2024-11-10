137c2d3c8e24efb152abfa080d6c9a7f6271aabf
Yamani <tasubilliyamani@gmail.com>
2024-11-10 19:07:14 +0530

    updated

--- BackEnd/expense_app/urls.py
+++ BackEnd/expense_app/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/login/',views.UserLoginView.as_view()),
-    path('api/signup/',views.UserSignupView.as_view()),
-    path('api/transaction/<int:pk>/',views.TransactionView.as_view()),
-    path('api/transaction/create/',views.TransactionCreateView.as_view()),
-    path('api/category/create/',views.CategoryCreateView.as_view()),
-    path('api/category/list',views.CategoryListView.as_view()),
-    path('api/categorytype/list/',views.CategoryTypeView.as_view()),
-    path('api/profile/',views.ProfileView.as_view()),
-    path('api/expenditure/',views.ExpenditureView.as_view()),
-    path('api/transaction/list/',views.TransactionView.as_view()),
-    path('api/transaction/delete/',views.TransactionDelete.as_view()),
+    path('login/',views.UserLoginView.as_view()),
+    path('signup/',views.UserSignupView.as_view()),
+    path('transaction/<int:pk>/',views.TransactionView.as_view()),
+    path('transaction/create/',views.TransactionCreateView.as_view()),
+    path('category/create/',views.CategoryCreateView.as_view()),
+    path('category/list',views.CategoryListView.as_view()),
+    path('categorytype/list/',views.CategoryTypeView.as_view()),
+    path('profile/',views.ProfileView.as_view()),
+    path('expenditure/',views.ExpenditureView.as_view()),
+    path('transaction/list/',views.TransactionView.as_view()),
+    path('transaction/delete/',views.TransactionDelete.as_view()),
 ]
     
