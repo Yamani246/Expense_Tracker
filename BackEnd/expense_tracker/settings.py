8425d68ac48d9d4af4d1d6f410f72e253b0f2961
Yamani <tasubilliyamani@gmail.com>
2024-11-14 18:17:52 +0530

    updated

--- BackEnd/expense_tracker/settings.py
+++ BackEnd/expense_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8gw*l4_l#3-kx)cebi!!357!wx6ik8+58(@!eq#!ro4!#t85ph
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vercel.app']
+ALLOWED_HOSTS = ['expense-tracker-backend-liard.vercel.app']
 
 
 # Application definition
